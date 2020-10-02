ba836e4cf88cb1cb187bfc9a65b286c077909247
Niclas O'Connor <oconnor@plt-software.de>
2020-10-02 13:12:15 +0200

    Improve layout

--- keyboards/kyria/keymaps/niclas/keymap.c
+++ keyboards/kyria/keymaps/niclas/keymap.c
@@ -31,21 +31,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Base Layer: QWERTY
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |  Tab   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  | \   |
+ * |  Tab   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |   =    |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |  Esc   |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |LShift|LShift|  |LShift|LShift|   N  |   M  | ,  < | . >  | /  ? |  - _   |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  | PGUp |LShift|  |LShift| PGDwn|   N  |   M  | ,  < | . >  | /  ? |  LShift - _   |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                        | GUI  |LAlt  | LCTL | BSPC | DEL  |  | Enter| Space| RAlt |  F5  |  F12 |
  *                        |      |      |      | Lower| Raise|  | Lower| Raise|      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-      KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PIPE,
-      KC_ESC,   KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_LSFT,   KC_LSFT, KC_LSFT, KC_LSFT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_LSFT, KC_MINS),
-                KC_LGUI, KC_LALT, KC_LCTL, LT(_LOWER, KC_BSPC), LT(_RAISE, KC_DEL), LT(_LOWER, KC_ENT), LT(_RAISE, KC_SPC), KC_RALT,  KC_F5, KC_F12
+      KC_TAB,   KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                                                                                     KC_Y,    KC_U,   KC_I,    KC_O,    KC_P,    KC_EQL,
+      KC_ESC,   KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                                                                                     KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+      KC_LSFT,  KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,    KC_PGUP,             KC_LSFT,            KC_LSFT,            KC_PGDN,            KC_N,     KC_M,  KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_LSFT, KC_MINS),
+                                KC_LGUI, KC_LALT, KC_LCTL, LT(_LOWER, KC_BSPC), LT(_RAISE, KC_DEL), LT(_LOWER, KC_ENT), LT(_RAISE, KC_SPC), KC_RALT,  KC_F5, KC_F12
     ),
 /*
  * Lower Layer: Symbols
