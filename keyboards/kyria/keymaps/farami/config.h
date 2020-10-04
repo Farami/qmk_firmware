155633bc6e327531a458ff4676cefd60e35500e9
Niclas O'Connor <oconnor@plt-software.de>
2020-10-04 09:50:54 +0200

    Enable rgb sleep on shutdown

--- keyboards/kyria/keymaps/farami/config.h
+++ keyboards/kyria/keymaps/farami/config.h
@@ -28,6 +28,8 @@
   #define RGBLIGHT_VAL_STEP 8
 
   #define RGBLIGHT_LED_MAP {0,1,2,9,8,7,4,3,5,6,19,18,17,10,11,12,15,16,14,13}
+
+  #define RGBLIGHT_SLEEP // turn off rgb when pc goes to sleep
 #endif
 
 
